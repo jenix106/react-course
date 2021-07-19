91e44d9426d52f1d4a1b8da26fc6ce7aee3d31d0
Ian Hooey <ian.hooey@gmail.com>
2021-07-19 17:41:13 -0500

    Added Firebase stuff, made the site look nice

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import {Route, Switch} from 'react-router-dom'
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return <div>
-    <MainNavigation/>
+  return <Layout>
     <Switch>
       <Route path='/' exact>
         <AllMeetupsPage/>
@@ -18,7 +17,7 @@ function App() {
         <FavoritesPage/>
       </Route>
     </Switch>
-  </div>;
+  </Layout>;
 }
 
 export default App;
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,44 @@
-function AllMeetupsPage(){
-  return <div>All Meetups Page</div>;
+import {useState, useEffect} from "react";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([])
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://react-getting-started-8b02a-default-rtdb.firebaseio.com/meetups.json').then(response => {
+      return response.json();
+    }).then(data => {
+      const meetups = [];
+
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        }
+
+        meetups.push(meetup)
+      }
+
+      setIsLoading(false);
+      setLoadedMeetups(meetups);
+    });
+
+    if (isLoading) {
+      return <section>
+        <p>Loading...</p>
+      </section>
+    }
+  }, []);
+
+
+
+  return <section>
+    <h1>All Meetups</h1>
+    <MeetupList meetups={loadedMeetups}/>
+  </section>;
 }
 
 export default AllMeetupsPage;
